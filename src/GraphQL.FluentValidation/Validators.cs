18ebca5ff0358b7ae8d546d12ff6d1147be7f342
Simon Cropp <simon.cropp@gmail.com>
2019-02-13 12:40:15 +1100

    add NotWhiteSpace

---
+++ src/GraphQL.FluentValidation/Validators.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using FluentValidation;
+
+namespace GraphQL.FluentValidation
+{
+    public static class Validators
+    {
+        public static void NotWhiteSpace<T>(this IRuleBuilderInitial<T, string> builder)
+        {
+            Guard.AgainstNull(builder, nameof(builder));
+            builder
+                .Must(u =>
+                {
+                    if (u == null)
+                    {
+                        return true;
+                    }
+
+                    return u.Any(char.IsWhiteSpace);
+                })
+                .WithMessage((type, member) => $"'{member}' must not be only whitespace.");
+        }
+    }
+}
