fadaf648fde34738dd891ec98e143499c31b2002
Simon Cropp <simon.cropp@gmail.com>
2019-03-05 09:09:29 +1100

    check for type cache in context

--- src/GraphQL.FluentValidation/ArgumentTypeCacheBag.cs
+++ src/GraphQL.FluentValidation/ArgumentTypeCacheBag.cs
@@ -1,11 +1,14 @@
-﻿using GraphQL.FluentValidation;
+﻿using System;
+using System.Collections.Generic;
+using GraphQL;
+using GraphQL.FluentValidation;
 using GraphQL.Types;
 
 static class ArgumentTypeCacheBag
 {
     public static ValidatorTypeCache GetCache(this ResolveFieldContext context)
     {
-        return (ValidatorTypeCache) context.Arguments["GraphQL.FluentValidation.ValidatorTypeCache"];
+        return GetCache(context.Arguments);
     }
 
     public static void SetCache(this ResolveFieldContext context, ValidatorTypeCache cache)
@@ -20,6 +23,16 @@ static class ArgumentTypeCacheBag
 
     public static ValidatorTypeCache GetCache<T>(ResolveFieldContext<T> context)
     {
-        return (ValidatorTypeCache) context.Arguments["GraphQL.FluentValidation.ValidatorTypeCache"];
+        return GetCache(context.Arguments);
+    }
+
+    static ValidatorTypeCache GetCache(Dictionary<string, object> arguments)
+    {
+        if (arguments.TryGetValue("GraphQL.FluentValidation.ValidatorTypeCache", out var result))
+        {
+            return (ValidatorTypeCache) result;
+        }
+
+        throw new Exception($"Could not extract {nameof(ValidatorTypeCache)} from ResolveFieldContext.Arguments. It is possible {nameof(FluentValidationExtensions)}.{nameof(FluentValidationExtensions.UseFluentValidation)} has not been called on {nameof(ExecutionOptions)}.");
     }
 }
