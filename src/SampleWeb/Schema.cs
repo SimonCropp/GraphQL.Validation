00ac7dbb506a84a42c3961763a2879706cc75d35
Simon Cropp <simon.cropp@gmail.com>
2021-03-22 10:09:49 +1100

    Update Schema.cs

--- src/SampleWeb/Schema.cs
+++ src/SampleWeb/Schema.cs
@@ -5,8 +5,8 @@ public class Schema : GraphQL.Types.Schema
     public Schema(IServiceProvider serviceProvider, Query query) :
         base(serviceProvider)
     {
-        RegisterTypeMapping(typeof(MyInput),typeof(MyInputGraph));
-        RegisterTypeMapping(typeof(Result),typeof(ResultGraph));
+        RegisterTypeMapping(typeof(MyInput), typeof(MyInputGraph));
+        RegisterTypeMapping(typeof(Result), typeof(ResultGraph));
         Query = query;
     }
 }
