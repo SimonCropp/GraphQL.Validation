768b2d113ddbf9db03003868dabbbfaffa625b5b
Simon Cropp <simon.cropp@gmail.com>
2022-05-10 20:13:55 +1000

    scrub Content-Length

--- src/SampleWeb.Tests/ModuleInitializer.cs
+++ src/SampleWeb.Tests/ModuleInitializer.cs
@@ -1,6 +1,12 @@
 public static class ModuleInitializer
 {
     [ModuleInitializer]
-    public static void Initialize() =>
+    public static void Initialize()
+    {
         VerifyHttp.Enable();
+        VerifierSettings.ModifySerialization(settings =>
+        {
+            settings.IgnoreMembers("Content-Length");
+        });
+    }
 }
